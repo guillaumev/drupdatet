9a0b467c7845906ae550217804e3a1c7fba1be2e
Guillaume Viguier <guillaume@viguierjust.com>
2015-12-17 11:46:10 +0100

    Made footer fluid

--- template.php
+++ template.php
@@ -7,3 +7,9 @@
 function drupdatet_preprocess_html(&$variables) {
   drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array('type' => 'external'));
 }
+
+function drupdatet_preprocess_region(&$variables, $hook) {
+  if($variables['region'] == "footer"){
+    $variables['classes_array'][] = 'container';
+  }
+}
