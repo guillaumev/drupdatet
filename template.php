02d56219e3e1655340f757f7d8384d6539eb32eb
Guillaume Viguier <guillaume@viguierjust.com>
2015-12-17 11:36:29 +0100

    Added fontawesome

--- template.php
+++ template.php
@@ -3,3 +3,7 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function drupdatet_preprocess_html(&$variables) {
+  drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array('type' => 'external'));
+}
